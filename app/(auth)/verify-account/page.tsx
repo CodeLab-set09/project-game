2daef579701f6a424d3e6d15d6c616b64bd2a198
TM-Code-009 <osakweterrynduka@gmail.com>
2024-07-29 14:55:34 +0100

    verify page done

--- app/(auth)/verify-account/page.tsx
+++ app/(auth)/verify-account/page.tsx
@@ -1,8 +1,14 @@
+import Link from 'next/link'
 import React from 'react'
 
 const page = () => {
   return (
-    <div>page</div>
+     <main className='w-full h-full flex justify-center items-center '>
+         <div className='h-[200px] w-[300px] flex flex-col rounded-md border items-center gap-5 p-4 '>
+            <p className='font-semibold text-[30px]'>Verify Account</p>
+            <p className='text-[16px] text-center '>An email has been sent to your email account<br/> <Link href={"/"} className='text-blue-500 hover:underline'>click to verify</Link></p>
+         </div>
+     </main>
   )
 }
 
