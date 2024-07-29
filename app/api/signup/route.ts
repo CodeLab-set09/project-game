fe0ab5c326328827f9a89640511c4c7f52571754
James418-alt <ebifegha123@gmail.com>
2024-07-29 14:52:21 +0100

    signup end point done

--- app/api/signup/route.ts
+++ app/api/signup/route.ts
@@ -2,6 +2,7 @@ import { dbConfig } from "@/utils/dbConfig";
 import { NextRequest, NextResponse } from "next/server";
 import bcrypt from "bcrypt";
 import userModel from "@/utils/model/userModel";
+import { AccountOpening } from "@/utils/email";
 
 export const POST = async (req: NextRequest) => {
   try {
@@ -13,7 +14,18 @@ export const POST = async (req: NextRequest) => {
     const rand = () => {
       return Math.floor(Math.random() * 1000000 + 1);
     };
-    console.log(rand);
+    const user = await userModel.create({
+      name,
+      email,
+      password: hashed,
+      verifyToken: rand(),
+    });
+    AccountOpening(user);
+    return NextResponse.json({
+      message: "User created",
+      status: 200,
+      data: user,
+    });
   } catch (error: any) {
     return NextResponse.json({
       message: "Error Occured",
