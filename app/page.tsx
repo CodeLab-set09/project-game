3f8ff06bd5d022ffcaadfa2ad73be004b08de9ac
PetxCode <brighterdayscodelab@gmail.com>
2024-07-29 20:01:26 +0100

    routing logic finally fixed

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import { getServerSession } from "next-auth";
-import React from "react";
+
 import { options } from "./api/auth/[...nextauth]/options";
 import { redirect } from "next/navigation";
 
