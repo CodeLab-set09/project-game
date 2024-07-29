369681ffedf109753b120b32255ae37256727150
PetxCode <brighterdayscodelab@gmail.com>
2024-07-29 13:48:39 +0100

    adding jwt

--- utils/constant.ts
+++ utils/constant.ts
@@ -1,1 +1,6 @@
 export const DB_URL = process.env.DB_STRING as string;
+
+export const GOOGLE_SECRET_KEY = process.env.GOOGLE_SECRET_KEY as string;
+export const GOOGLE_CLIENT = process.env.GOOGLE_CLIENT as string;
+export const GOOGLE_URL = process.env.GOOGLE_URL as string;
+export const GOOGLE_REFRESH_TOKEN = process.env.GOOGLE_REFRESH_TOKEN as string;
---
+++ utils/email.ts
@@ -0,0 +1,7 @@
+import { google } from "googleapis";
+import nodemailer from "nodemailer";
+
+const GOOGLE_SECRET_KEY = "";
+const GOOGLE_CLIENT = "";
+const GOOGLE_URL = "";
+const GOOGLE_REFRESH_TOKEN = "";
