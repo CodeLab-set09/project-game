0ce114edce14d63e7ccaf23e9f9af515fe4033ed
odinaka12 <odinakaahamba@gmail.com>
2024-07-31 18:04:18 +0100

    update on email tem

--- utils/email.ts
+++ utils/email.ts
@@ -51,15 +51,14 @@ export const AccountOpeningEmail = async (user: any) => {
       subject: "Account Opening",
       text: "Welcome to our platform!",
       html: `
-
-    <head>
+      <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="x-apple-disable-message-reformatting">
     <title></title>
 
-    <link href="https://fonts.googleapis.com/css?family=Poppins:400,600" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:400,600" rel="stylesheet" type="text/css">
     
 
     <style>
@@ -69,7 +68,7 @@ export const AccountOpeningEmail = async (user: any) => {
             padding: 0 !important;
             height: 100% !important;
             width: 100% !important;
-            font-family: 'Poppins', sans-serif !important;
+            font-family: 'Roboto', sans-serif !important;
             font-size: 14px;
             margin-bottom: 10px;
             line-height: 24px;
@@ -120,17 +119,8 @@ export const AccountOpeningEmail = async (user: any) => {
                         <tbody>
                             <tr>
                                 <td style="text-align: center; padding-bottom:25px">
-
-                                    
-
-                                   <img 
-                                   alt="logo"
-                                   src="https://res.cloudinary.com/duewdl1ua/image/upload/v1722432802/codebook_black_w1f3i7.png"
-                                   width="500px"
-                                   height="200px"
-                                   object-fit="contain"
-                                   />
-
+                                    <img style="height: 40px" src="https://res.cloudinary.com/duewdl1ua/image/upload/v1722432802/codebook_black_w1f3i7.png"
+                                            alt="logo">
                                 </td>
                             </tr>
                         </tbody>
@@ -140,42 +130,26 @@ export const AccountOpeningEmail = async (user: any) => {
                             <tr>
                                 <td style="padding: 30px 30px 15px 30px;">
                                     <h2 style="font-size: 18px; color: #141414; font-weight: 600; margin: 0;">Verify
-                                        Your Account</h2>
+                                        Your E-Mail Address</h2>
                                 </td>
                             </tr>
                             <tr>
                                 <td style="padding: 0 30px 20px">
-<<<<<<< HEAD
-                                    <p style="margin-bottom: 10px; color: #080808">Welcome! <br> You are receiving this email because
+                                    <p style="margin-bottom: 10px; color:#080808 ">Hi ${user?.userName},</p>
+                                    <p style="margin-bottom: 10px; color:#080808">Welcome! <br> You are receiving this email because
                                         you have registered on our site.</p>
-                                    <p style="margin-bottom: 10px; color: #080808">Click the button below to active your code book
+                                    <p style="margin-bottom: 10px; color:#080808">Click the button below to active your account.
                                         account.</p>
-                                    <p style="margin-bottom: 25px; color: #080808">Here is your sign up token: ${user.verifyToken}</p>
-                                    <a href="${url}"
-                                        style="background-color:#141414;border-radius:4px;color:#ffffff;display:inline-block;font-size:13px;font-weight:600;line-height:44px;text-align:center;text-decoration:none;text-transform: uppercase; padding: 0 30px">Verify
-=======
-                                    <p style="margin-bottom: 10px;">Hi ${user?.userName},</p>
-                                    <p style="margin-bottom: 10px;">Welcome! <br> You are receiving this email because
-                                        you have registered on our site.</p>
-                                    <p style="margin-bottom: 10px;">Click the link below to active your CodeBook
-                                        Account.</p>
-                                    <p style="margin-bottom: 25px;">This link will expire in 15 minutes and can only be
-                                        used once. and here is your OTP code
-                                        <p 
-                                        style="color:#0f172a;font-size:14px; margin:"10px";font-weight:600;">${user?.verifyToken}</p>
-                                        </p>
+                                    <p style="margin-bottom: 25px;  color:#080808">This is your verification token: ${user?.verifyToken} </p>
                                     <a href="${LIVE_URL}/verify-account/${token}"
-                                        style="background-color:#6576ff;border-radius:4px;color:#ffffff;display:inline-block;font-size:13px;font-weight:600;line-height:44px;text-align:center;text-decoration:none;text-transform: uppercase; padding: 0 30px">Verify
->>>>>>> 7259b740a3308c61b74ffa0d1cefa13b220dcba9
+                                        style="background-color:#141414;border-radius:4px;color:#ffffff;display:inline-block;font-size:13px;font-weight:600;line-height:44px;text-align:center;text-decoration:none;text-transform: uppercase; padding: 0 30px">Verify
                                         Email</a>
+                                        
                                 </td>
                             </tr>
                             <tr>
-                                <td style="padding: 0 30px">
-                                    <h4
-                                        style="font-size: 15px; color: #000000; font-weight: 600; margin: 0; text-transform: uppercase; margin-bottom: 10px">
-                                        or</h4>
-                                    <p style="margin-bottom: 10px;">If the button above does not work, paste this link
+                                <td style="padding: 10px 30px ">
+                                    <p style="color:#080808;">If the button above does not work, paste this link
                                         into your web browser:</p>
                                     <a href="${LIVE_URL}/verify-account/${token}"
                                         style="color: #6576ff; text-decoration:none;word-break: break-all;">${LIVE_URL}/verify-account/${token}</a>
@@ -183,33 +157,23 @@ export const AccountOpeningEmail = async (user: any) => {
                             </tr>
                             <tr>
                                 <td style="padding: 20px 30px 40px">
-<<<<<<< HEAD
-                                    <p style="color: #080808">If you did not make this request, please contact us or ignore this message.</p>
+                                    <p style=" color:#080808">If you did not make this request, please contact us or ignore this message.</p>
                                     <p style="margin: 0; font-size: 13px; line-height: 22px; color:#adadaf;">This is an automatically generated
                                         email please do not reply to this email. If you face any issues, please contact us at
-                                        codelab@gmail.com</p>
-=======
-                                    <p>If you did not make this request, please contact us or ignore this message.</p>
-                                    <p style="margin: 0; font-size: 13px; line-height: 22px; color:#9ea8bb;">This is an
-                                        automatically generated email please do not reply to this email. If you face any
-                                        issues, 
-                                        <br/>
-                                        <br/>
-                                        <br/>
-                                        
-                                        please contact us at codebook@gmail.com</p>
->>>>>>> 7259b740a3308c61b74ffa0d1cefa13b220dcba9
+                                        <a href="#">codelab@gmail.com</a></p>
                                 </td>
                             </tr>
                            
-                        </tbody> 
+                        </tbody>
                     </table>
                     
                 </td>
             </tr>
         </table>
     </main>
-</body>`,
+</body>
+
+      `,
     };
 
     transport.sendMail(mailOptions).then(() => {
