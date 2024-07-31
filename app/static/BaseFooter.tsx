a972441a89e1580a101d8aca0b130d27760c0a5c
James418-alt <ebifegha123@gmail.com>
2024-07-31 16:07:21 +0100

    update on the footer

--- app/static/BaseFooter.tsx
+++ app/static/BaseFooter.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { FaSquareXTwitter, FaLinkedin } from "react-icons/fa6";
 import { FaFacebookSquare } from "react-icons/fa";
 import Image from "next/image";
+import Link from "next/link";
 
 const BaseFooter = () => {
   const social = [
@@ -22,30 +23,46 @@ const BaseFooter = () => {
     },
   ];
   const languages = [
-    { id: 0, name: "Javascript" },
-    { id: 1, name: "HTML" },
-    { id: 2, name: "Python" },
-    { id: 3, name: "PHP" },
+    { id: 0, name: "Javascript", url: "#" },
+    { id: 1, name: "HTML", url: "#" },
+    { id: 2, name: "Python", url: "#" },
+    { id: 3, name: "PHP", url: "#" },
   ];
   return (
-    <main className="border-t pt-20 px-16">
+    <main className="border-t pt-20 px-6 md:px-16">
       <section className="flex flex-col gap-3">
         <Image src={"/cbb.png"} alt="#" width={160} height={160} />
-        <h2 className="text-[15px] font-semibold ">Top Languages</h2>
-      </section>
-
-      <section>
-        <div>2024</div>
-        <div>2024</div>
-      </section>
-
-      <section>
-        {social?.map((el) => (
-          <div key={el.id} className="">
-            {el.icon}
+        <div className="flex flex-col gap-2">
+          <h2 className="text-[15px] font-semibold ">Top Languages</h2>
+          <div className="flex gap-1 flex-wrap">
+            {languages.map((el: any) => (
+              <div key={el.id}>
+                <Link
+                  className="rounded-full bg-gray-400 py-1 px-2 text-[10px] font-semibold text-white"
+                  href={el.url}
+                >
+                  {el.name}
+                </Link>
+              </div>
+            ))}
           </div>
-        ))}
+        </div>
       </section>
+
+      <div className="flex items-center justify-between border-t border-neutral-400 py-10 mt-14">
+        <section className="flex flex-col gap-10 items-center text-neutral-500">
+          <div>@2024 CodelabSet09. All right reserved</div>
+          <div>Powered by CodeLab</div>
+        </section>
+
+        <section className="flex items-center gap-3">
+          {social?.map((el) => (
+            <div key={el.id} className="text-[25px]">
+              {el.icon}
+            </div>
+          ))}
+        </section>
+      </div>
     </main>
   );
 };
