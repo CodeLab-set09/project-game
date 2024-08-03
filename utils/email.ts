2e8124a8b88874baf05edca640d7e0ad68da8a2d
odinaka12 <odinakaahamba@gmail.com>
2024-08-03 07:55:41 +0100

    update on forget pass email

--- utils/email.ts
+++ utils/email.ts
@@ -138,8 +138,7 @@ export const AccountOpeningEmail = async (user: any) => {
                                     <p style="margin-bottom: 10px; color:#080808 ">Hi ${user?.userName},</p>
                                     <p style="margin-bottom: 10px; color:#080808">Welcome! <br> You are receiving this email because
                                         you have registered on our site.</p>
-                                    <p style="margin-bottom: 10px; color:#080808">Click the button below to active your account.
-                                        account.</p>
+                                    <p style="margin-bottom: 10px; color:#080808">Click the button below to active your account.</p>
                                     <p style="margin-bottom: 25px;  color:#080808">This is your verification token: ${user?.verifyToken} </p>
                                     <a href="${LIVE_URL}/verify-account/${token}"
                                         style="background-color:#141414;border-radius:4px;color:#ffffff;display:inline-block;font-size:13px;font-weight:600;line-height:44px;text-align:center;text-decoration:none;text-transform: uppercase; padding: 0 30px">Verify
@@ -210,13 +209,122 @@ export const resetPasswordEmail = async (user: any) => {
       subject: "Reset Account Password",
       text: "Welcome to our platform!",
       html: `
-        <div>
-        <h1>Welcome to our platform!</h1>
-        <p>You've successfully created an account.</p>
-        <p>Here is your sign up token: ${user.verifyToken}</p>
-        <p>Please click the link below to sign in:</p>
-        <a href="${url}">Sign In</a>
-        </div>`,
+      <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="x-apple-disable-message-reformatting">
+    <title></title>
+
+    <link href="https://fonts.googleapis.com/css?family=Roboto:400,600" rel="stylesheet" type="text/css">
+
+    <style>
+        html,
+        body {
+            margin: 0 auto !important;
+            padding: 0 !important;
+            height: 100% !important;
+            width: 100% !important;
+            font-family: 'Roboto', sans-serif !important;
+            font-size: 14px;
+            margin-bottom: 10px;
+            line-height: 24px;
+            color: #141414;
+            font-weight: 400;
+        }
+
+        * {
+            -ms-text-size-adjust: 100%;
+            -webkit-text-size-adjust: 100%;
+            margin: 0;
+            padding: 0;
+        }
+
+        table,
+        td {
+            mso-table-lspace: 0pt !important;
+            mso-table-rspace: 0pt !important;
+        }
+
+        table {
+            border-spacing: 0 !important;
+            border-collapse: collapse !important;
+            table-layout: fixed !important;
+            margin: 0 auto !important;
+        }
+
+        table table table {
+            table-layout: auto;
+        }
+
+        a {
+            text-decoration: none;
+        }
+
+        img {
+            -ms-interpolation-mode: bicubic;
+        }
+    </style>
+
+</head>
+
+<body width="100%" style="margin: 0; padding: 0 !important; mso-line-height-rule: exactly; background-color: #f5f6fa;">
+    <center style="width: 100%; background-color: #f5f6fa;">
+        <table width="100%" border="0" cellpadding="0" cellspacing="0" bgcolor="#f5f6fa">
+            <tr>
+                <td style="padding: 40px 0;">
+                    <table style="width:100%;max-width:620px;margin:0 auto;">
+                        <tbody>
+                            <tr>
+                                <td style="text-align: center; padding-bottom:25px">
+                                    <a href="#"><img style="height: 40px" src="https://res.cloudinary.com/duewdl1ua/image/upload/v1722432802/codebook_black_w1f3i7.png"
+                                            alt="logo"></a>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <table style="width:100%;max-width:620px;margin:0 auto;background-color:#ffffff;">
+                        <tbody>
+                            <tr>
+                                <td style="text-align:center;padding: 30px 30px 15px 30px;">
+                                    <h2 style="font-size: 18px; color: #141414; font-weight: 600; margin: 0;">Reset
+                                        Password</h2>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td style="text-align:center;padding: 0 30px 20px">
+                                    <p style="margin-bottom: 10px;">Hi ${user?.userName},</p>
+                                    <p style="margin-bottom: 10px;">Click On The button blow to reset your password.</p>
+                                    <p style="margin-bottom: 25px;">This is your verification token: ${user?.verifyToken}</p>
+                                    <a href="${url}"
+                                        style="background-color:#141414;border-radius:4px;color:#ffffff;display:inline-block;font-size:13px;font-weight:600;line-height:44px;text-align:center;text-decoration:none;text-transform: uppercase; padding: 0 25px">Reset
+                                        Password</a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td style="text-align:center;padding: 0 30px 20px ">
+                                    <p style="color:#080808;">If the button above does not work, paste this link
+                                        into your web browser:</p>
+                                    <a href="${LIVE_URL}/verify-account/${token}"
+                                        style="color: #6576ff; text-decoration:none;word-break: break-all;">${LIVE_URL}/verify-account/${token}</a>
+                                </td>
+                            <tr>
+                                <td style="text-align:center;padding: 20px 30px 40px">
+                                    <p>If you did not make this request, please contact us or ignore this message.</p>
+                                    <p style="margin: 0; font-size: 13px; line-height: 22px; color:#9ea8bb;">This is an
+                                        automatically generated email please do not reply to this email. If you face any
+                                        issues, please contact us at <a href="#" style="color:#6576ff">codebook@gmail.com</a></p>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    
+                </td>
+            </tr>
+        </table>
+    </center>
+</body>
+        `,
     };
 
     transport.sendMail(mailOptions).then(() => {
