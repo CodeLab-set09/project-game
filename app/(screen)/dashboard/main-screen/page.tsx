edfbf286e78bd016b49d97b29cd69313dd88a71a
James418-alt <ebifegha123@gmail.com>
2024-08-05 12:55:17 +0100

    changing mainscreen

--- app/(screen)/dashboard/main-screen/page.tsx
+++ app/(screen)/dashboard/main-screen/page.tsx
@@ -1,13 +1,11 @@
 "use client";
 
 import { changeUserTimer } from "@/app/apiCalls/apiCall";
-import { LIVE_URL } from "@/utils/constant";
+
 import { useSession } from "next-auth/react";
-import { useRouter } from "next/navigation";
+
 import React, { useEffect } from "react";
-import Challenge from "./Challenge";
-import Badge from "./Badge";
-import MainPage from "../MainPage";
+import MainPage from "./MainPage";
 
 const Page = () => {
   const session: any = useSession();
