d3fb6ad239e1a1891d7ec2075b7e7f74c488951f
PetxCode <brighterdayscodelab@gmail.com>
2024-08-05 13:08:38 +0100

    main page

--- app/(screen)/dashboard/MainPage.tsx
+++ app/(screen)/dashboard/MainPage.tsx
@@ -1,17 +1,7 @@
 import React from "react";
-import { MdBadge, MdSearch, MdSettings } from "react-icons/md";
-import Challenge from "../../components/Challenge";
-import { BiBadge } from "react-icons/bi";
-import { FaAward, FaCertificate } from "react-icons/fa";
-import Badge from "../../components/Badge";
 
 const MainPage = () => {
-  return (
-    <main>
-      <Challenge />
-      <Badge />
-    </main>
-  );
+  return <div>MainPage</div>;
 };
 
 export default MainPage;
