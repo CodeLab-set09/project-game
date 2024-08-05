8d5583b477d6ce36b4c630b7247634d8204124b4
James418-alt <ebifegha123@gmail.com>
2024-08-05 12:51:02 +0100

    update on the main-screen

--- app/(screen)/dashboard/main-screen/page.tsx
+++ app/(screen)/dashboard/main-screen/page.tsx
@@ -1,7 +1,31 @@
-import React from "react";
+"use client";
 
-const page = () => {
-  return <div>This is the main start up screen</div>;
+import { changeUserTimer } from "@/app/apiCalls/apiCall";
+import { LIVE_URL } from "@/utils/constant";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import React, { useEffect } from "react";
+import Challenge from "./Challenge";
+import Badge from "./Badge";
+import MainPage from "../MainPage";
+
+const Page = () => {
+  const session: any = useSession();
+  const id = session?.data?.user?.id;
+
+  useEffect(() => {
+    if (id) {
+      changeUserTimer(id).then((res) => {
+        console.log("res: ", res);
+      });
+    }
+  }, [id]);
+
+  return (
+    <div>
+      <MainPage />
+    </div>
+  );
 };
 
-export default page;
+export default Page;
