5b58ec6eeaee45dd9757899ae6a890530d777ab1
PetxCode <brighterdayscodelab@gmail.com>
2024-08-07 16:44:45 +0100

    great

--- app/(screen)/landing-page/page.tsx
+++ app/(screen)/landing-page/page.tsx
@@ -3,14 +3,16 @@ import Hero from "./Hero";
 import Sharpencomp from "./Sharpencomp";
 import Section3 from "./Section3";
 import Section4 from "./Section4";
+import { MainHero } from "./MainHero";
 
 const page = () => {
   return (
     <div>
+      <MainHero />
       <Hero />
       <Sharpencomp />
-      <Section3/>
-      <Section4/>
+      <Section3 />
+      <Section4 />
     </div>
   );
 };
