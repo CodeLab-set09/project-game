d9b5ac3baef00a8d41657dc6716f5f46c35129a7
PetxCode <brighterdayscodelab@gmail.com>
2024-08-08 10:32:03 +0100

    great

--- declaration.d.ts
+++ declaration.d.ts
@@ -1,2 +1,4 @@
 declare module "*mongoose";
-declare module "*@/public/assets/";
+declare module "@/public/assets/right.png";
+declare module "@/public/assets/right.png";
+declare module "@/public/assets/right.png";
