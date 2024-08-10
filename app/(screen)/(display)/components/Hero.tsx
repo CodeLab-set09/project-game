12be7d8aa4ca9b577cfcc61b7f275b2361ce9f09
PetxCode <brighterdayscodelab@gmail.com>
2024-08-10 22:41:23 +0100

    greate

--- app/(screen)/(display)/components/Hero.tsx
+++ app/(screen)/(display)/components/Hero.tsx
@@ -5,11 +5,17 @@ import MessageImage from "../assets/images/message.png";
 import Image from "next/image";
 import { motion } from "framer-motion";
 import { AnimatedGradientTextDemo } from "./animatedtext";
+import Particles from "../../landing-page/lib/Particles";
 
 export const Hero = () => {
   return (
     <div className="bg-black text-white bg-[linear-gradient(to_bottom,#000,#200D42_34%,#4F21A1_65%,#A46EDB_82%)] py-[72px] sm:py-24 relative overflow-clip">
+      <Particles
+        sizeValue={5}
+        className="absolute inset-0 pointer-events-none"
+      />
       <div className="absolute h-[375px] w-[750px] sm:w-[1536px] sm:h-[768px] lg:w-[2400px] llg:h-[800px] rounded-[100%] bg-black left-1/2 -translate-x-1/2 border border-[#B48CDE] bg-[radial-gradient(closest-side,#000_82%,#9560EB)] top-[calc(100%-96px)] sm:top-[calc(100%-120px)]"></div>
+
       <div className="container relative">
         <div className="flex items-center justify-center -mt-10">
           <AnimatedGradientTextDemo />
@@ -17,10 +23,12 @@ export const Hero = () => {
         <div className="flex justify-center mt-8 ">
           <div className="inline-flex relative">
             <h1 className="text-7xl sm:text-9xl font-bold tracking-tightner text-center inline-flex">
-              Codify <br /> is more
+              Codify By
+              <br />
+              CodeLab
             </h1>
             <motion.div
-              className="absolute right-[478px] top-[108px] hidden sm:inline"
+              className="absolute right-[578px] top-[108px] hidden sm:inline"
               drag
               dragSnapToOrigin
             >
@@ -30,11 +38,11 @@ export const Hero = () => {
                 height={200}
                 width={200}
                 className="max-w-none"
-                draggable="false"
+                draggable="true"
               />
             </motion.div>
             <motion.div
-              className="absolute left-[498px] top-[56px] hidden sm:inline"
+              className="absolute left-[598px] top-[56px] hidden sm:inline"
               drag
               dragSnapToOrigin
             >
