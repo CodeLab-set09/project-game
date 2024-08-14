da720dda2f525373371a5c8ed20c1923a4f845cb
zakariyaumuahmmed18@gmail.com <zakariyaumuahmmed18@gmail.comgit config --global user.email zakariyaumuahmmed18@gmail.comgit config --global user.email zakariyaumuahmmed18@gmail.com>
2024-08-14 06:13:19 -0700

    blog form done

--- app/(screen)/(cms)/blog/page.tsx
+++ app/(screen)/(cms)/blog/page.tsx
@@ -1,26 +1,39 @@
+"use client"
+
+import { Toaster } from '@/components/ui/toaster';
+import { toast, useToast } from '@/components/ui/use-toast'
 import { LIVE_URL } from '@/utils/constant'
-import React from 'react'
+import React, { CSSProperties, useState } from 'react'
+import BeatLoader from 'react-spinners/BeatLoader';
 
 const page = () => {
+
+  const override: CSSProperties = {
+    display: "block",
+    margin: "0 auto",
+    borderColor: "red",
+  };
+    
+  const [loading, setLoading] = useState(false);
 
    const createblog = async(formdata:FormData) => {
      
     const title = formdata.get("title")
-    const image = formdata.get("title")
-    const video = formdata.get("title")
-    const content = formdata.get("title")
+    const image = formdata.get("image")
+    const video = formdata.get("video")
+    const content = formdata.get("content")
 
 
 
     await fetch(`${LIVE_URL}/api/Blog` ,{
-
        method: 'POST',
        headers: {
-         'Content-Type': 'application/json'
+         'content-Type': 'application/json'
        },
        body: JSON.stringify({title, image, video, content})
  
     })
+  
    }
 
   return (
@@ -31,29 +44,45 @@ const page = () => {
 
             <div className='w-full mt-6 '><hr /></div>
 
-            <form action="" className='mt-5 flex flex-col gap-5'>
+            <form action={createblog} className='mt-5 flex flex-col gap-5'>
+
                 <div  >
                     <label className='font-semibold text-[16px]'>Title</label>
-                    <input type="text" placeholder='title' className='w-full h-[40px] p-2 border outline-none ' />
+                    <input type="text" name="title" placeholder='title' className='w-full h-[40px] p-2 border outline-none ' />
                 </div>
 
                 <div>
                     <label className='font-semibold text-[16px]'>Image</label>
-                    <input type="text" placeholder='image' className='w-full h-[40px] p-2 border outline-none ' />
+                    <input type="text" name="image" placeholder='image' className='w-full h-[40px] p-2 border outline-none ' />
                 </div>
 
                 <div>
                     <label className='font-semibold text-[16px]'>Video</label>
-                    <input type="text" placeholder='video' className='w-full h-[40px] p-2 border outline-none ' />
+                    <input type="text" name="video" placeholder='video' className='w-full h-[40px] p-2 border outline-none ' />
                 </div>
                 <div>
                     <label className='font-semibold text-[16px]'>Content</label>
-                    <textarea name="" placeholder='content' className='h-[150px] w-full border outline-none resize-none p-2 '></textarea>
+                    <textarea name="content" placeholder='content' className='h-[150px] w-full border outline-none resize-none p-2 '></textarea>
                 </div>
 
-                <button type='submit' className='w-full h-[40px] bg-blue-700 font-semibold text-white ' >
-                    Submit
-                </button>
+                <button
+            type="submit"
+            onClick={() => setLoading(true)}
+            className="p-1 bg-blue-950 py-3 text-white rounded-md font-bold mt-4 "
+          >
+            {loading ? (
+              <BeatLoader
+                color="#fff"
+                loading={loading}
+                cssOverride={override}
+                size={10}
+                aria-label="Loading Spinner"
+                data-testid="loader"
+              />
+            ) : (
+              "Submit"
+            )}{" "}
+          </button>
             </form>
 
          </div>
