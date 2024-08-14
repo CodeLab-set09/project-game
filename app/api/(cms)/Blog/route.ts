542aa56fd88b4aec60216ec19d0b65a7dccd1272
ashele1234 <calebashele@gmail.com>
2024-08-14 11:05:32 +0100

    committing

--- app/api/(cms)/Blog/route.ts
+++ app/api/(cms)/Blog/route.ts
@@ -7,9 +7,9 @@ import { NextRequest, NextResponse } from "next/server"
 export const POST =async(req:NextRequest)=>{
 try {
     await dbConfig()
-    const {title,video,content } = await req.json()
+    const {title,video,content,desc,image } = await req.json()
 
-    const blog = blogdata.create({title,video,content })
+    const blog = blogdata.create({title,video,content,desc,image })
     return NextResponse.json({
         status:200,
         message:"blog created successfully",
@@ -42,3 +42,23 @@ try {
       })
     }
 }
+export const GET =async(req:NextRequest)=>{
+  try {
+      await dbConfig()
+  
+      const blog = await blogdata.find()
+  
+      return NextResponse.json({
+          status:200,
+          message:"reading blogs",
+          data:blog
+        })
+  
+      } catch (error) {
+        return NextResponse.json({
+          status:404,
+          message:"Error reading blogs"
+        })
+      }
+  }
+  
