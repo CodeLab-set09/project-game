5aceb69c8be81948caf74c0e874c5e5e2f4ae818
odinaka12 <odinakaahamba@gmail.com>
2024-08-15 11:05:37 +0100

    odinaka

--- app/(screen)/landing-page/blog/page.tsx
+++ app/(screen)/landing-page/blog/page.tsx
@@ -4,7 +4,7 @@ import { LIVE_URL } from "@/utils/constant";
 const page = async() => {
    const res = await fetch(`${LIVE_URL}/api/Blog`,{
      method:"GET",
-     cache:"no-cache",
+     cache:"no-cache", 
      next:{
       tags:["blog"]
     }
@@ -17,7 +17,7 @@ const page = async() => {
           data?.data?.map((el:any) => (
             <div key={el._id} className=" bg-slate-500 rounded-[15px]">
 
-              <div className="text-[30px] h-[200px] w-full bg-black rounded-t-[15px] rounded-tr-[15px]"><img src={el.image}/></div>
+              <div className="text-[30px] h-[200px] w-full bg-black rounded-t-[15px] rounded-tr-[15px] object-fill"><img src={el.image}/></div>
               <div className="w-full px-5 py-8">
               <p className="text-[25px] pb-2 font-medium">{el.title}</p>
               <p className="text-balance text-[15px] font-normal"> hello{el.desc}</p>
